1ef8f2310e8b81a28263a001ea276279f6fb9f09
Tanguy Foujols <tanguy.foujols@icloud.com>
2018-02-14 15:51:38 +0100

    Yelp MVP OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+ {
+   name:         'Chez Gladines',
+   address:      '25 boulevard batignolles, Paris',
+   category:     'french'
+ },
+ {
+   name:         'Tchang',
+   address:      'Tokyo, Japon',
+   category:     'japanese',
+   phone_number: '0668295116'
+ },
+ {
+   name:         'Pizza East',
+   address:      '56A Shoreditch High St, London E1 6PQ',
+   category:     'italian',
+   phone_number: '0668695113'
+ },
+ {
+   name:         'Jeon Guy',
+   address:      'Shangaï, China',
+   category:     'chinese',
+   phone_number: '0668695116'
+ },
+ {
+   name:         'Bistrot café',
+   address:      'rue saint maur, Paris',
+   category:     'french'
+ }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
